1d296d2b0f78d8cab12b3d238849f51851205fa9
tenglai <13049322087@163.com>
2018-11-20 22:50:56 +0800

    init

--- config/index.js
+++ config/index.js
@@ -1,11 +1,14 @@
 const config = {
-  projectName: 'taro-msparis',
-  date: '2018-9-27',
-  // 设计稿尺寸
+  projectName: 'myApp',
+  date: '2018-11-20',
   designWidth: 750,
+  deviceRatio: {
+    '640': 2.34 / 2,
+    '750': 1,
+    '828': 1.81 / 2
+  },
   sourceRoot: 'src',
   outputRoot: 'dist',
-  // 通用插件配置
   plugins: {
     babel: {
       sourceMap: true,
@@ -18,17 +21,49 @@ const config = {
         'transform-object-rest-spread'
       ]
     },
+    typescript: {
+      compilerOptions: {
+        allowSyntheticDefaultImports: true,
+        baseUrl: '.',
+        declaration: false,
+        experimentalDecorators: true,
+        jsx: 'preserve',
+        jsxFactory: 'Nerv.createElement',
+        module: 'commonjs',
+        moduleResolution: 'node',
+        noImplicitAny: false,
+        noUnusedLocals: true,
+        outDir: './dist/',
+        preserveConstEnums: true,
+        removeComments: false,
+        rootDir: '.',
+        sourceMap: true,
+        strictNullChecks: true,
+        target: 'es6'
+      },
+      include: [
+        'src/**/*'
+      ],
+      exclude: [
+        'node_modules'
+      ],
+      compileOnSave: false
+    }
   },
-  // 全局变量设置
-  defineConstants: {},
-  // 小程序端专用配置
+  defineConstants: {
+  },
+  copy: {
+     patterns: [
+    ],
+    options: {
+    }
+  },
   weapp: {
     module: {
       postcss: {
         autoprefixer: {
           enable: true
         },
-        // 小程序端样式引用本地资源内联配置
         url: {
           enable: true,
           limit: 10240
@@ -36,7 +71,6 @@ const config = {
       }
     }
   },
-  // H5 端专用配置
   h5: {
     publicPath: '/',
     staticDirectory: 'static',
--- src/app.js
+++ src/app.js
@@ -1,75 +1,35 @@
-import '@tarojs/async-await'
 import Taro, { Component } from '@tarojs/taro'
-import Home from './pages/home'
-import dva from './utils/dva'
-import models from './models'
-import { Provider } from '@tarojs/redux'
+import Index from './pages/index'
 
-import './styles/base.scss'
-
-
-const dvaApp = dva.createApp({
-  initialState: {},
-  models: models,
-});
-const store = dvaApp.getStore();
+import './app.scss'
 
 class App extends Component {
 
   config = {
     pages: [
-      'pages/home/index',
-      'pages/cart/index',
-      'pages/user/index',
-      'pages/detail/index',
-      'pages/about/index',
-      'pages/size/index',
-      'pages/login/index',
-      'pages/message/index',
-      'pages/couponList/index',
-      'pages/order/index',
-      'pages/addressList/index',
-      'pages/addressUpdate/index'
+      'pages/index/index'
     ],
     window: {
-      backgroundTextStyle: 'dark',
+      backgroundTextStyle: 'light',
       navigationBarBackgroundColor: '#fff',
-      navigationBarTitleText: '时装衣橱',
+      navigationBarTitleText: 'WeChat',
       navigationBarTextStyle: 'black'
-    },
-    tabBar: {
-      list: [{
-        pagePath: "pages/home/index",
-        text: "首页",
-        iconPath: "./images/tab/home.png",
-        selectedIconPath: "./images/tab/home-active.png"
-      }, {
-        pagePath: "pages/cart/index",
-        text: "衣袋",
-        iconPath: "./images/tab/cart.png",
-        selectedIconPath: "./images/tab/cart-active.png"
-      },{
-        pagePath: "pages/user/index",
-        text: "我的",
-        iconPath: "./images/tab/user.png",
-        selectedIconPath: "./images/tab/user-active.png"
-      }],
-      color: '#333',
-      selectedColor: '#333',
-      backgroundColor: '#fff',
-      borderStyle: '#ccc'
     }
   }
 
-  componentDidMount() {
+  componentDidMount () {}
 
-  }
+  componentDidShow () {}
 
-  render() {
-    return (<Provider store={store}>
-      <Home/>
-    </Provider>);
+  componentDidHide () {}
+
+  componentCatchError () {}
+
+  render () {
+    return (
+      <Index />
+    )
   }
 }
 
-Taro.render(<App/>, document.getElementById('app'))
+Taro.render(<App />, document.getElementById('app'))
